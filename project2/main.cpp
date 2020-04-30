ea0daf70d29338bb57b090a86045db7c1e5710f6
Chih-Yu Kao <lcd010308@gmail.com>
2020-04-30 21:59:57 +0800

    Refactor basic backtrack search

--- project2/main.cpp
+++ project2/main.cpp
@@ -6,22 +6,20 @@ int board[100][100];
 char c_board[100][100];
 const int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-struct Domain {
-    int x, y, value;
-    Domain(int x, int y, int value = 0b11) : x(x), y(y), value(value) {}
-};
+string heuristic_type;
 
 struct Node {
     vector<vector<int>> assignments;
-    list<Domain> domains;
+    vector<vector<int>> domains;
+    list<pair<int, int>> unassigned;
 
     Node () {
         assignments.resize(board_size_x, vector<int>(board_size_y, 0));
-        domains.clear();
+        domains.resize(board_size_x, vector<int>(board_size_y, -1));
+        unassigned.clear();
     }
-    Node (const vector<vector<int>>& assignments, const list<Domain>& domains)
-        : assignments(assignments), domains(domains) {}
+    Node (const vector<vector<int>>& assignments, const vector<vector<int>>& domains, const list<pair<int, int>> unassigned)
+        : assignments(assignments), domains(domains), unassigned(unassigned) {}
 };
 
 bool is_outside(int x, int y) {
@@ -53,6 +51,53 @@ void output(const vector<vector<int>>& assignments) {
     cout << "======================\n";
 }
 
+void init_root(Node& node) {
+    for (int x = 0; x < board_size_x; x++) {
+        for (int y = 0; y < board_size_y; y++) {
+            if (board[x][y] == -1) {
+                node.assignments[x][y] = -1;
+                node.unassigned.push_back(make_pair(x, y));
+            }
+        }
+    }
+
+    for (int x = 0; x < board_size_x; x++) {
+        for (int y = 0; y < board_size_y; y++) {
+            if (board[x][y] != -1) continue;
+
+            int current_domain = 0b11;
+            for (int i = 0; i < 8; i++) {
+                int center_x = x + dx[i];
+                int center_y = y + dy[i];
+                if (is_outside(center_x, center_y))   continue;
+                if (board[center_x][center_y] == -1)  continue;
+
+                int mine_num = 0;
+                int space_num = 0;
+                for (int j = 0; j < 8; j++) {
+                    int outer_x = center_x + dx[j];
+                    int outer_y = center_y + dy[j];
+                    if (is_outside(outer_x, outer_y))   continue;
+
+                    if (board[outer_x][outer_y] == -1) {
+                        if (node.assignments[outer_x][outer_y] == 1)    mine_num++;
+                        if (node.assignments[outer_x][outer_y] == -1)   space_num++;
+                    }
+                }
+
+                int mine_need = board[center_x][center_y] - mine_num;
+                if (mine_need == 0) current_domain &= 0b01;
+                else if (mine_need == space_num) current_domain &= 0b10;
+                else if (mine_need < space_num) current_domain &= 0b11;
+                else if (mine_need > space_num) current_domain &= 0b00;
+
+                if (current_domain == 0b00) break;
+            }
+            node.domains[x][y] = current_domain;
+        }
+    }
+}
+
 void backtrack_search(const Node& root) {
     int num_expand = 0;
     stack<Node> frontier;
@@ -60,78 +105,89 @@ void backtrack_search(const Node& root) {
 
     while (!frontier.empty()) {
         Node node = frontier.top(); frontier.pop();
-        if (node.domains.empty()) {
+        if (node.unassigned.empty()) {
             output(node.assignments);
             continue;
         }
 
         num_expand++;
-        Domain domain = node.domains.front();
-        node.domains.pop_front();
-        int current_domain = 0b11;
-        for (int i = 0; i < 8; i++) {
-            int center_x = domain.x + dx[i];
-            int center_y = domain.y + dy[i];
-            if (is_outside(center_x, center_y))   continue;
-            if (board[center_x][center_y] == -1)  continue;
+        pair<int, int> variable = node.unassigned.front();
+        node.unassigned.pop_front();
 
-            // cout << center_x << " " << center_y << "\n";
+        int x = variable.first, y = variable.second;
+        int current_domain = node.domains[x][y];
+        if (current_domain == 0)  continue;
 
-            int mine_num = 0;
-            int space_num = 0;
-            for (int j = 0; j < 8; j++) {
-                int outer_x = center_x + dx[j];
-                int outer_y = center_y + dy[j];
-                if (is_outside(outer_x, outer_y))   continue;
+        for (int k = 0; k <= 1; k++) {
+            if ((current_domain & (1 << k)) == 0)   continue;
 
-                if (board[outer_x][outer_y] == -1) {
-                    if (node.assignments[outer_x][outer_y] == 1)    mine_num++;
-                    if (node.assignments[outer_x][outer_y] == -1)   space_num++;
+            vector<vector<int>> assignments = node.assignments;
+            vector<vector<int>> domains = node.domains;
+            list<pair<int, int>> unassigned = node.unassigned;
+            assignments[x][y] = k;
+
+            for (int i = 0; i < 8; i++) {
+                int center_x = x + dx[i];
+                int center_y = y + dy[i];
+                if (is_outside(center_x, center_y))   continue;
+                if (board[center_x][center_y] == -1)  continue;
+
+                // cout << center_x << " " << center_y << "\n";
+
+                int mine_num = 0;
+                int space_num = 0;
+                for (int j = 0; j < 8; j++) {
+                    int outer_x = center_x + dx[j];
+                    int outer_y = center_y + dy[j];
+                    if (is_outside(outer_x, outer_y))   continue;
+
+                    if (board[outer_x][outer_y] == -1) {
+                        if (assignments[outer_x][outer_y] == 1)    mine_num++;
+                        if (assignments[outer_x][outer_y] == -1)   space_num++;
+                    }
+                }
+                int mine_need = board[center_x][center_y] - mine_num;
+                int update_domain;
+                // cout << board[center_x][center_y] << " " << mine_num << " " << space_num << "\n"; 
+                if (mine_need == 0)              update_domain = 0b01;
+                else if (mine_need == space_num) update_domain = 0b10;
+                else if (mine_need < space_num)  update_domain = 0b11;
+                else if (mine_need > space_num)  update_domain = 0b00;
+
+                for (int j = 0; j < 8; j++) {
+                    int outer_x = center_x + dx[j];
+                    int outer_y = center_y + dy[j];
+                    if (is_outside(outer_x, outer_y))   continue;
+
+                    if (board[outer_x][outer_y] == -1) {
+                        domains[outer_x][outer_y] &= update_domain;
+                    }
                 }
             }
-            int mine_need = board[center_x][center_y] - mine_num;
-            // cout << board[center_x][center_y] << " " << mine_num << " " << space_num << "\n"; 
-            if (mine_need == 0) current_domain &= 0b01;
-            else if (mine_need == space_num) current_domain &= 0b10;
-            else if (mine_need < space_num) current_domain &= 0b11;
-            else if (mine_need > space_num) current_domain &= 0b00;
-
-            if (current_domain == 0b00) break;
-        }
-
-        vector<vector<int>> assignments = node.assignments;
-        list<Domain> domains = node.domains;
-        if (current_domain & 0b10) {
-            assignments[domain.x][domain.y] = 1;
-            frontier.push(Node{assignments, domains});
-        }
-        if (current_domain & 0b01) {
-            assignments[domain.x][domain.y] = 0;
-            frontier.push(Node{assignments, domains});
+            frontier.push(Node{assignments, domains, unassigned});
         }
     }
+
     cout << "Number of solutions: " << solution_num << "\n";
     cout << "Number of expanded nodes: " << num_expand << "\n";
 }
 
-int main(int argc, char **argv) {
-    while (cin >> board_size_x >> board_size_y >> mine_total) {
-        Node root;
-        for (int i = 0; i < board_size_x; i++) {
-            for (int j = 0; j < board_size_y; j++) {
-                cin >> board[i][j];
-                if (board[i][j] == -1) {
-                    root.assignments[i][j] = -1;
-                    root.domains.emplace_back(i, j, 0b11);
-                }
-            }
-        }
+int main(int argc, char **argv) { // 8 2 9 2
+    if (argc > 1) heuristic_type = argv[1];
 
-        auto t1 = std::chrono::high_resolution_clock::now();
-        backtrack_search(root);
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-        cout << duration << " us\n";
+    cin >> board_size_x >> board_size_y >> mine_total;
+    Node root;
+    for (int i = 0; i < board_size_x; i++) {
+        for (int j = 0; j < board_size_y; j++) {
+            cin >> board[i][j];
+        }
     }
+    init_root(root);
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    backtrack_search(root);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    cout << duration << " us\n";
     return 0;
 }
